f38e182f68944830546f261824d6241e05c6c21f
Nilo Almeida <niloalmeidadev@gmail.com>
2023-08-11 10:08:20 -0300

    ADcionando package.json padrão

--- generate-ms.sh
+++ generate-ms.sh
@@ -8,12 +8,10 @@ npm init -y
 cd "source"
 mkdir constants controllers services errors routes validations
 cd ..
-npm install -D typescript nodemon
-npm i express zod @prisma/client @types/express bcryptjs @types/bcryptjs dotenv prisma uuid 
 
 echo "import express from 'express';
 import 'dotenv/config'
-import routes from './source/routes/routes';
+import { routes } from './source/routes/routes';
 
 const app = express();
 app.use(express.json());
@@ -22,5 +20,10 @@ app.use(routes());
 app.listen(process.env.PORT, ()=>{console.log('Server is on in port '+process.env.PORT)})" >> server.ts
 cd /home/ryladant/boilerplate/microservico/ms-generate-default/
 cp ./tsconfig.json ../"ms-$1-default"/
+cp ./.env ../"ms-$1-default"/
 cp -r ./default_files/routes/ ../"ms-$1-default"/source/
+cp ./package.json ../"ms-$1-default"/
+cd /home/ryladant/boilerplate/microservico/ms-$1-default/
+npm install
+npx tsc
 
