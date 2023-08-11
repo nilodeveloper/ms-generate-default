9e775eb7a217e6272ff443fa7b7f76206cf4c079
Nilo Almeida <niloalmeidadev@gmail.com>
2023-08-11 08:50:50 -0300

    First commit

---
+++ generate-ms.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ..
+mkdir "ms-$1-default"
+cd "ms-$1-default"
+mkdir "source"
+npm init -y
+cd "source"
+mkdir constants controllers services errors routes validations
+cd ..
+npm i expressjs typescript nodemon tsc zod @prisma/client @types/express bcryptjs @types/bcryptjs dotenv prisma uuid 
+
+echo "import express from 'express';
+import 'dotenv/config'
+import routes from './source/routes/routes';
+
+const app = express();
+app.use(express.json());
+app.use(routes());
+
+app.listen(process.env.PORT, ()=>{console.log('Server is on in port '+process.env.PORT)})" >> server.ts
+cd "/home/ryladant/boilerplate/microservico/ms-generate-default/"
+cp ./tsconfig.json ../"ms-$1-default"/
+
